1a4c6c8db5fd30e3afc9ae5cdebd1f5ca4baed49
Chiharu-Hagihara <chiharuhagihara803@gmail.com>
2020-07-16 19:45:14 +0900

    initial commit

---
+++ src/main/java/mc/mec/serverpoint/ServerPoint.kt
@@ -0,0 +1,13 @@
+package mc.mec.serverpoint
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class ServerPoint : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
