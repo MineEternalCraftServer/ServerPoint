1fb83b2240e30e781e7bec694cc738f752505013
owner <forgotten_saga_1129@outlook.jp>
2020-07-23 21:09:20 +0900

    管理メソッドの追加

--- src/main/java/mc/mec/serverpoint/PointAPI.kt
+++ src/main/java/mc/mec/serverpoint/PointAPI.kt
@@ -1,5 +1,7 @@
 package mc.mec.serverpoint
 
+import java.util.*
+
 /**
  * Created by testusuke on 2020/07/23
  * @author testusuke
@@ -10,5 +12,61 @@ package mc.mec.serverpoint
  */
 object PointAPI {
 
+    /**
+     * function of create player data
+     * @param uuid[String]　UUID
+     * @return success[Boolean] success
+     */
+    fun createPlayer(uuid: String):Boolean{
+
+
+        return true
+    }
+
+    /**
+     * function of delete player data
+     * @param uuid[String]　UUID
+     * @return success[Boolean] success
+     */
+    fun deletePlayer(uuid: String):Boolean{
+
+
+        return true
+    }
+
+    /**
+     * function of add point
+     * @param uuid[String] uuid
+     * @param amount[Int] point amount
+     * @return success[Boolean] success
+     */
+    fun addPoint(uuid:String,amount:Int):Boolean{
+
+
+        return true
+    }
+
+    /**
+     * function of remove point
+     * @param uuid[String] uuid
+     * @param amount[Int] point amount
+     * @return success[Boolean] success
+     */
+    fun removePoint(uuid:String,amount:Int):Boolean{
+
+
+        return true
+    }
+
+    /**
+     * function of get point
+     * @param uuid[String] uuid
+     * @return point[Int] point (if throw exception,return Int -1)
+     */
+    fun getPoint(uuid:String):Int{
+        var amount = 0
+
+        return amount
+    }
 
 }
