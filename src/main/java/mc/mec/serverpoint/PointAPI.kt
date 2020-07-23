8c7ceb4a700e853234cf641e95fd3909f2ccc7d7
owner <forgotten_saga_1129@outlook.jp>
2020-07-24 07:05:20 +0900

    PointAPI.ktの実装

--- src/main/java/mc/mec/serverpoint/PointAPI.kt
+++ src/main/java/mc/mec/serverpoint/PointAPI.kt
@@ -2,7 +2,12 @@ package mc.mec.serverpoint
 
 import mc.mec.serverpoint.ServerPoint.Companion.dataBase
 import mc.mec.serverpoint.ServerPoint.Companion.plugin
+import org.bukkit.scheduler.BukkitRunnable
+import java.sql.Connection
 import java.sql.SQLException
+import java.sql.Statement
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 import java.util.*
 
 /**
@@ -21,7 +26,29 @@ object PointAPI {
      * @return success[Boolean] success
      */
     fun createPlayer(uuid: String):Boolean{
+        val sql = "INSERT INTO server_point (uuid,point,date) VALUES ('${uuid}','0','${getDate()}');"
+        try {
+            //  DB
+            val connection = dataBase.getConnection()
+            if (connection == null) {
+                dataBase.sendErrorMessage()
+                plugin.logger.info("Can not access DB. method:PointAPI/createPlayer")
+                return false
+            }
+            val statement = connection.createStatement()
+            statement.execute(sql)
+            statement.close()
+            connection.close()
 
+            //  Log
+            addLog(LogInfo(uuid,Action.DELETE,-1, getDate()))
+            //  Logger
+            plugin.logger.info("created player. uuid:$uuid")
+        }catch (e:SQLException){
+            e.printStackTrace()
+            plugin.logger.info("Can not create player.")
+            return false
+        }
         return true
     }
 
@@ -44,6 +71,9 @@ object PointAPI {
             statement.execute(sql)
             statement.close()
             connection.close()
+
+            //  Log
+            addLog(LogInfo(uuid,Action.DELETE,-1, getDate()))
             //  Logger
             plugin.logger.info("deleted player. uuid:$uuid")
         }catch (e:SQLException){
@@ -61,8 +91,56 @@ object PointAPI {
      * @return success[Boolean] success
      */
     fun addPoint(uuid:String,amount:Int):Boolean{
+        //  Select
+        val selectSQL = "SELECT point,date FROM server_point WHERE uuid='${uuid}' LIMIT 1;"
+        //  UPDATE
+        val updateSQL = "UPDATE server_point SET point='_POINT_',date='_DATE_';"
 
+        try {
+            //  DB
+            val connection = dataBase.getConnection()
+            if (connection == null) {
+                dataBase.sendErrorMessage()
+                plugin.logger.info("Can not access DB. method:PointAPI/addPoint")
+                return false
+            }
+            val statement = connection.createStatement()
+            val result = statement.executeQuery(selectSQL)
+            var point:Int = if(!result.next()){
+                //  Create Player Data
+                createPlayer(uuid)
+                0
+            }else{
+                var p = 0
+                while (result.next()){
+                    p = result.getInt("point")
+                }
+                p
+            }
+            //  close resultSet
+            result.close()
 
+            //  add point
+            point += amount
+            //  replace
+            updateSQL.replace("_POINT_",point.toString())
+            updateSQL.replace("_DATE_",getDate())
+            //  Execute
+            statement.executeUpdate(updateSQL)
+
+            //  Close
+            statement.close()
+            connection.close()
+
+            //  Log
+            addLog(LogInfo(uuid,Action.ADD,amount, getDate()))
+            //  Logger
+            plugin.logger.info("added point. uuid:$uuid added: $amount")
+        }catch (e:SQLException){
+            e.printStackTrace()
+            plugin.logger.info("Can not add point.")
+            return false
+        }
         return true
     }
 
@@ -73,8 +151,55 @@ object PointAPI {
      * @return success[Boolean] success
      */
     fun removePoint(uuid:String,amount:Int):Boolean{
+        //  Select
+        val selectSQL = "SELECT point,date FROM server_point WHERE uuid='${uuid}' LIMIT 1;"
+        //  UPDATE
+        val updateSQL = "UPDATE server_point SET point='_POINT_',date='_DATE_';"
 
+        try {
+            //  DB
+            val connection = dataBase.getConnection()
+            if (connection == null) {
+                dataBase.sendErrorMessage()
+                plugin.logger.info("Can not access DB. method:PointAPI/removePoint")
+                return false
+            }
+            val statement = connection.createStatement()
+            val result = statement.executeQuery(selectSQL)
+            var point:Int = if(!result.next()){
+                //  Create Player Data
+                createPlayer(uuid)
+                0
+            }else{
+                var p = 0
+                while (result.next()){
+                    p = result.getInt("point")
+                }
+                p
+            }
+            //  close resultSet
+            result.close()
 
+            //  add point
+            point -= amount
+            //  replace
+            updateSQL.replace("_POINT_",point.toString())
+            updateSQL.replace("_DATE_", getDate())
+            //  Execute
+            statement.executeUpdate(updateSQL)
+            //  Close
+            statement.close()
+            connection.close()
+
+            //  Log
+            addLog(LogInfo(uuid,Action.REMOVE,amount, getDate()))
+            //  Logger
+            plugin.logger.info("added point. uuid:$uuid added: $amount")
+        }catch (e:SQLException){
+            e.printStackTrace()
+            plugin.logger.info("Can not add point.")
+            return false
+        }
         return true
     }
 
@@ -85,7 +210,40 @@ object PointAPI {
      */
     fun getPoint(uuid:String):Int{
         var amount = 0
+        //  Select
+        val selectSQL = "SELECT point,date FROM server_point WHERE uuid='${uuid}' LIMIT 1;"
 
+        try {
+            //  DB
+            val connection = dataBase.getConnection()
+            if (connection == null) {
+                dataBase.sendErrorMessage()
+                plugin.logger.info("Can not access DB. method:PointAPI/getPoint")
+                return -0
+            }
+            val statement = connection.createStatement()
+            val result = statement.executeQuery(selectSQL)
+            amount = if(!result.next()){
+                //  Create Player Data
+                createPlayer(uuid)
+                0
+            }else{
+                var p = 0
+                while (result.next()){
+                    p = result.getInt("point")
+                }
+                p
+            }
+            //  close resultSet
+            result.close()
+            //  Close
+            statement.close()
+            connection.close()
+        }catch (e:SQLException){
+            e.printStackTrace()
+            plugin.logger.info("Can not get point data.")
+            return -1
+        }
         return amount
     }
 
@@ -126,5 +284,61 @@ object PointAPI {
             return false
         }
         return true
+    }
+
+    private enum class Action{
+        CREATE,DELETE,ADD,REMOVE
+    }
+    private data class LogInfo(val uuid: String,val action:Action,val point:Int,val date:String)
+    //////////////////////////////
+    //  LOG
+    /**
+     * function of add logs.
+     * @param info[LogInfo] information data class
+     */
+    private fun addLog(info:LogInfo){
+        object : BukkitRunnable() {
+            override fun run() {
+                val sql = when(info.action){
+                    Action.CREATE -> {
+                        "INSERT INTO server_point (uuid,action,point,date) VALUES ('${info.uuid}','CREATE','-1','${info.date}');"
+                    }
+                    Action.DELETE -> {
+                        "INSERT INTO server_point (uuid,action,point,date) VALUES ('${info.uuid}','DELETE','-1','${info.date}');"
+                    }
+                    Action.ADD -> {
+                        "INSERT INTO server_point (uuid,action,point,date) VALUES ('${info.uuid}','ADD','${info.point}','${info.date}');"
+                    }
+                    Action.REMOVE -> {
+                        "INSERT INTO server_point (uuid,action,point,date) VALUES ('${info.uuid}','REMOVE','${info.point}','${info.date}');"
+                    }
+                }
+                try {
+                    //  DB
+                    val connection = dataBase.getConnection()
+                    if (connection == null) {
+                        dataBase.sendErrorMessage()
+                        plugin.logger.info("Can not access DB. method:PointAPI/addLog")
+                        return
+                    }
+                    val statement = connection.createStatement()
+                    statement.execute(sql)
+                    statement.close()
+                    connection.close()
+                }catch (e:SQLException){
+                    e.printStackTrace()
+                    plugin.logger.info("Can not add log.")
+                }
+            }
+        }.runTask(plugin)
+    }
+
+    //  Unit
+    //  get date
+    private fun getDate():String{
+        //  date
+        val current = LocalDateTime.now()
+        val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+        return current.format(formatter)
     }
 }
